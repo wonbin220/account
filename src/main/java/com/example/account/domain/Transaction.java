2f531d68880adc2fd7d6f44e0c7e3f3214a8f5ed
wonbin220 <wonbin220@gmail.com>
2024-06-24 22:03:39 +0900

    upload

---
+++ src/main/java/com/example/account/domain/Transaction.java
@@ -0,0 +1,35 @@
+package com.example.account.domain;
+
+import com.example.account.type.TransactionResultType;
+import com.example.account.type.TransactionType;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class Transaction extends BaseEntity {
+    @Enumerated(EnumType.STRING)
+    private TransactionType transactionType;
+    @Enumerated(EnumType.STRING)
+    private TransactionResultType transactionResultType;
+
+    @ManyToOne
+    private Account account;
+    private Long amount;
+    private Long balanceSnapshot;
+
+    @CreatedDate
+    private String transactionId;
+    @LastModifiedDate
+    private LocalDateTime transactedAt;
+}
---
+++ src/main/java/com/example/account/dto/UseBalance.java
@@ -0,0 +1,59 @@
+package com.example.account.dto;
+
+import com.example.account.aop.AccountLockIdInterface;
+import com.example.account.type.TransactionResultType;
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+public class UseBalance {
+    /**
+     * {
+     *     "userId": 1,
+     *     "accountNumber":"1000000000",
+     *     "amount":1000
+     * }
+     */
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    public static class Request implements AccountLockIdInterface {
+        @NotNull
+        @Min(1)
+        private Long userId;
+
+        @NotBlank
+        @Size(min = 10, max = 10)
+        private String accountNumber;
+
+        @NotNull
+        @Min(10)
+        @Max(1000_000_000)
+        private Long amount;
+
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class Response {
+        private String accountNumber;
+        private TransactionResultType transactionResult;
+        private String transactionId;
+        private Long amount;
+        private LocalDateTime registeredAt;
+
+        public static Response from(TransactionDto transactionDto) {
+            return Response.builder()
+                    .accountNumber(transactionDto.getAccountNumber())
+                    .transactionResult(transactionDto.getTransactionResultType())
+                    .transactionId(transactionDto.getTransactionId())
+                    .amount(transactionDto.getAmount())
+                    .registeredAt(transactionDto.getTransactedAt())
+                    .build();
+        }
+    }
+}
